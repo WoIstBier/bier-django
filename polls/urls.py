18b245189851de618677c23d561fecf687dc1052
MacKaiver <mackaiver@Novalee-II-Ubuntu.(none)>
2013-02-22 18:20:41 +0100

    tried to use seom regex and failed

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,8 @@
-'''
-Created on Feb 17, 2013
-
-@author: mackaiver
-'''
 from django.conf.urls import patterns, url
 
 from polls import views
 
 urlpatterns = patterns('',
+    url(r'^polls/(?P<kiosk_id>\d+)/$', 'polls.views.biere'),
     url(r'^$', views.index, name='index'),
 )
