3b5ecf4fcbcc1ff3e8b930e2dab76f92b3ebc59f
Daniel Smit <daniel.smit@udo.edu>
2013-02-23 12:40:51 +0100

    renamed from 'polls' to 'bier'

--- bier/urls.py
+++ bier/urls.py
@@ -5,7 +5,7 @@ Created on Feb 17, 2013
 '''
 from django.conf.urls import patterns, url
 
-from polls import views
+from bier import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
--- bier/views.py
+++ bier/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from polls.models import Kiosk, BierPreisListe
+from bier.models import Kiosk, BierPreisListe
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -7,12 +7,12 @@ from django.http import HttpResponse
 def index(request):
     kiosk_liste = Kiosk.objects.order_by('name')
     context = {'kioske': kiosk_liste}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'bier/index.html', context)
 
 def preise(request):
     bier_liste = BierPreisListe.objects.order_by('preis')
     context = {'bier_liste': bier_liste}
-    return render(request, 'polls/bier.html', context)
+    return render(request, 'bier/bier.html', context)
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")
