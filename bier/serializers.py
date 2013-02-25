7be498508f591363df2f3c09a776f4ccda6fb6cf
Daniel Smit <daniel.smit@udo.edu>
2013-02-25 17:34:58 +0100

    adapted the REST-serializer to the new model

--- bier/serializers.py
+++ bier/serializers.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 class KioskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Kiosk
-        fields = ('id', 'name', 'address', 'owner')
+        fields = ('id', 'name', 'street', 'number', 'zip_code', 'city', 'owner', 'geo_lat', 'geo_long')
 
