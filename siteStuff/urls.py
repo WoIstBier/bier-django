06ba39b63228ec14262e4b6fcae760d458a9edd2
bier <bier@cepheus.uberspace.de>
2013-03-13 14:25:07 +0100

    bla

--- siteStuff/urls.py
+++ siteStuff/urls.py
@@ -17,11 +17,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #include the urls from the polls app.
     url(r'^bier/', include('bier.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': MEDIA_ROOT,
-    }),
-    url(r'^static/(?P<path>.*)$','django.views.static.serve',{
-        'document_root': '/home/bier/html/static'
-    }),
+#    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+ #       'document_root': MEDIA_ROOT,
+  #  }),
+   # url(r'^static/(?P<path>.*)$','django.views.static.serve',{
+    #    'document_root': '/home/bier/html/static'
+   # }),
     url(r'^$', 'bier.views.index', name='kiosk')
 )
