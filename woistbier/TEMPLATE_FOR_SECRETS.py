8701992bb2588074a45a1653bd286e8ed518e0cc
Kai <kai.bruegge@gmail.com>
2016-10-23 14:45:17 +0200

    add template for secrets so people know whats supposed to go in there

---
+++ woistbier/TEMPLATE_FOR_SECRETS.py
@@ -0,0 +1,4 @@
+# Make this unique, and don't share it with anybody.
+django_secret_key = 'djangos secret key. A random 50 charachter string.'
+uberspace_mail_password = 'the ubersapce mail password'
+uberspace_mysql_password = 'your_mysql_password'
